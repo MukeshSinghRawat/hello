c39820b9aedb27f3e4c194dd892d92ba6ae4ca53
Muk007 <38641966+Muk007@users.noreply.github.com>
2023-03-16 13:19:23 +0530

    Update execute_ddl_and_dml_js_scripts.py

--- scripts/python/execute_ddl_and_dml_js_scripts.py
+++ scripts/python/execute_ddl_and_dml_js_scripts.py
@@ -17,7 +17,7 @@ js_list.append(os.environ['JS_PATH_2'])
 
 def execute_js():
   for paths in js_list:
-    execution = "node"+paths
+    execution = "node "+paths
     os.system(execution)
 
 execute_js()
