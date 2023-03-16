9d3a8c8f3f23241458ab681d00145942badbd400
Muk007 <38641966+Muk007@users.noreply.github.com>
2023-03-16 12:11:06 +0530

    Update execute_ddl_and_dml_js_scripts.py

--- scripts/python/execute_ddl_and_dml_js_scripts.py
+++ scripts/python/execute_ddl_and_dml_js_scripts.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 
-# import the modules
+# import modules
 import os
-# import sys 
-# import json
-# import pymongo
+
 
 mongo_url = os.environ['MY_SECRET_HOSTNAME']
 
-def print_hostname(hostname):
-  new_hostname = hostname + '-custom'
-  print(new_hostname)
-  
-print_hostname(mongo_url)
+print(mongo_url)
