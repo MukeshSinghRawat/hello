ee945e68a6b1381c78e84b234036b33a8c434ca4
Muk007 <38641966+Muk007@users.noreply.github.com>
2023-03-16 13:09:22 +0530

    Update execute_ddl_and_dml_js_scripts.py

--- scripts/python/execute_ddl_and_dml_js_scripts.py
+++ scripts/python/execute_ddl_and_dml_js_scripts.py
@@ -8,8 +8,8 @@ mongo_username = os.environ['MY_SECRET_USERNAME']
 print(mongo_username)
 
 # block 2
-javascript_path_1=os.environment['JS_PATH_1']
-javascript_path_2=os.environment['JS_PATH_2']
+javascript_path_1=os.environ['JS_PATH_1']
+javascript_path_2=os.environ['JS_PATH_2']
 
 def execute_js(js_path_1, js_path_2):
   execution = "node "+js_path_1+" \nnode "+js_path_2
