70c58c8a2ec1f6e6a2153c668542651ed86c79a6
Muk007 <38641966+Muk007@users.noreply.github.com>
2023-03-22 11:39:48 +0530

    Update script1.js

--- scripts/javascripts/script1.js
+++ scripts/javascripts/script1.js
@@ -1,4 +1,4 @@
-db = db.getSiblingDB('ONE');
-db.createCollection('CSS.masterData');
-db.createCollection('CSS.Wires.masterData');
-db.createCollection('CSS.InternationalWires.masterData');
+db = db.getSiblingDB('MUKESH');
+db.createCollection('CSS.MUKESH');
+db.createCollection('CSS.Wires.MUKESH');
+db.createCollection('CSS.InternationalWires.MUKESH');
