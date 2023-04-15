8eb2451e14123fd4d23a546b03fa881741f1e7c5
Muk007 <38641966+Muk007@users.noreply.github.com>
2023-04-15 21:30:19 +0530

    Update execute-ddl-dml.py

--- devops/execute-ddl-dml.py
+++ devops/execute-ddl-dml.py
@@ -9,14 +9,21 @@ import os
 
 # Run the js script on the mongo
 js_list = []
-js_list.append(os.environ['JS_PATH_1'])
-js_list.append(os.environ['JS_PATH_2'])
-container_id = os.environ['CONTAINER_ID']
+js_list.append(os.environ['DDL_SCRIPT_PATH'])
+js_list.append(os.environ['DML_SCRIPT_PATH'])
+auth_mechanism = os.environ['AUTH_MECHANISM']
+mongo_connection_string = os.environ['MONGO_CONNECTION_STRING']
+tls_certificate_key_file = os.environ['TLS_CERTIFICATE_KEY_FILE']
+tls_certificate_key_file_password = os.environ['TLS_CERTIFICATE_KEY_FILE_PASSWORD']
+# container_id = os.environ['CONTAINER_ID']
 
-def execute_js(con_id, list):
+# def execute_js(con_id, list):
+def execute_js(list):
   for paths in list:
-    basename = os.path.basename(paths)
-    execution = "docker cp "+paths+" "+con_id+":/"+basename+"\ndocker exec "+con_id+" mongosh "+basename+"\ndocker exec "+con_id+" mongosh --eval 'db.getMongo().getDBNames()'"
+#     basename = os.path.basename(paths)
+#     execution = "docker cp "+paths+" "+con_id+":/"+basename+"\ndocker exec "+con_id+" mongosh "+basename+"\ndocker exec "+con_id+" mongosh --eval 'db.getMongo().getDBNames()'"
+    execution = ###############################
     os.system(execution)   
     
-execute_js(container_id, js_list)
+# execute_js(container_id, js_list)
+execute_js(js_list)
