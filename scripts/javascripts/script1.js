f78236e2e463373fe5bac9fd34827c5539525d9e
Muk007 <38641966+Muk007@users.noreply.github.com>
2023-04-15 20:38:20 +0530

    Delete scripts directory

--- scripts/javascripts/script1.js
+++
@@ -1,4 +0,0 @@
-db = db.getSiblingDB('MUKESH');
-db.createCollection('CSS.MUKESH');
-db.createCollection('CSS.Wires.MUKESH');
-db.createCollection('CSS.InternationalWires.MUKESH');
--- scripts/javascripts/script2.js
+++
@@ -1,4 +0,0 @@
-db = db.getSiblingDB('ABDUL');
-db.createCollection('CSS.ABDUL');
-db.createCollection('CSS.Wires.ABDUL');
-db.createCollection('CSS.InternationalWires.ABDUL');
