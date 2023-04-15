204dd0cff18c26d90110148ce20cddbc5b2ec7ff
Muk007 <38641966+Muk007@users.noreply.github.com>
2023-04-15 22:14:37 +0530

    Update execute-ddl-dml.py

--- devops/execute-ddl-dml.py
+++ devops/execute-ddl-dml.py
@@ -3,11 +3,7 @@
 # import modules
 import os
 
-# Testing the env variables
-# mongo_username = os.environ['MY_SECRET_USERNAME']
-# print(mongo_username)
-
-# Run the js script on the mongo
+# Import all variables exposed to the environment
 js_list = []
 js_list.append(os.environ['DDL_SCRIPT_PATH'])
 js_list.append(os.environ['DML_SCRIPT_PATH'])
@@ -15,7 +11,6 @@ auth_mechanism = os.environ['AUTH_MECHANISM']
 mongo_connection_string = os.environ['MONGO_CONNECTION_STRING']
 tls_certificate_key_file = os.environ['TLS_CERTIFICATE_KEY_FILE']
 tls_certificate_key_file_password = os.environ['TLS_CERTIFICATE_KEY_FILE_PASSWORD']
-# container_id = os.environ['CONTAINER_ID']
 
 # def execute_js(con_id, list):
 def execute_js(list):
