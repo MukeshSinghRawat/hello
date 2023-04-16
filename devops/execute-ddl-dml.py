626faf8c8d55b79240168da52837f398fafb3dc2
Muk007 <38641966+Muk007@users.noreply.github.com>
2023-04-16 09:16:26 +0530

    Update execute-ddl-dml.py

--- devops/execute-ddl-dml.py
+++ devops/execute-ddl-dml.py
@@ -4,26 +4,36 @@
 import os
 
 # Importing all variables exposed to the environment
-list = []
-list.append(os.environ['DDL_SCRIPT_PATH'])
-list.append(os.environ['DML_SCRIPT_PATH'])
+# list = []
+# list.append(os.environ['DDL_SCRIPT_PATH'])
+# list.append(os.environ['DML_SCRIPT_PATH'])
+
+ddl_script_path = os.environ['DDL_SCRIPT_PATH']
+dml_script_path = os.environ['DML_SCRIPT_PATH']
 auth_mechanism = os.environ['AUTH_MECHANISM']
 mongo_connection_string = os.environ['MONGO_CONNECTION_STRING']
 tls_certificate_key_file = os.environ['TLS_CERTIFICATE_KEY_FILE']
 tls_certificate_key_file_password = os.environ['TLS_CERTIFICATE_KEY_FILE_PASSWORD']
 
-def execute_js(mongo_connection_string, tls_certificate_key_file, tls_certificate_key_file_password, auth_mechanism, list):
-  for paths in list:
-    if os.path.exists(paths):
-      shell_command = "mongosh '"+mongo_connection_string+"' --tls --tlsCertificateKeyFile "+tls_certificate_key_file+" --tlsCertificateKeyFilePassword "+tls_certificate_key_file_password+" --authenticationMechanism "+auth_mechanism < paths
-      os.system(shell_command)   
-    else:
-      print(paths+" doesn't exist...")
-      
+# Define functions to execute ddl and dml
+def execute_ddl(mongo_connection_string, tls_certificate_key_file, tls_certificate_key_file_password, auth_mechanism, ddl_script_path):
+  if os.path.exists(ddl_script_path):
+    ddl_shell_command = "mongosh '"+mongo_connection_string+"' --tls --tlsCertificateKeyFile "+tls_certificate_key_file+" --tlsCertificateKeyFilePassword "+tls_certificate_key_file_password+" --authenticationMechanism "+auth_mechanism < ddl_script_path
+    os.system(ddl_shell_command)   
+  else:
+    print(ddl_script_path+" doesn't exist...")
     
-# Calling the 'execute_js' function
+def execute_dml(mongo_connection_string, tls_certificate_key_file, tls_certificate_key_file_password, auth_mechanism, dml_script_path):
+  if os.path.exists(dml_script_path):
+    dml_shell_command = "mongosh '"+mongo_connection_string+"' --tls --tlsCertificateKeyFile "+tls_certificate_key_file+" --tlsCertificateKeyFilePassword "+tls_certificate_key_file_password+" --authenticationMechanism "+auth_mechanism < dml_script_path
+    os.system(dml_shell_command)   
+  else:
+    print(dml_script_path+" doesn't exist...")
+  
+# Calling the functions
 try:
-  execute_js(mongo_connection_string, tls_certificate_key_file, tls_certificate_key_file_password, auth_mechanism, list)
+  execute_ddl(mongo_connection_string, tls_certificate_key_file, tls_certificate_key_file_password, auth_mechanism, ddl_script_path)
+  execute_dml(mongo_connection_string, tls_certificate_key_file, tls_certificate_key_file_password, auth_mechanism, dml_script_path)
  
 except:
   print("script execution failed...")
